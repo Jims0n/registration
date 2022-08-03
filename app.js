89dd1163429ef7ef3967f406cb8efc7a49f2872c
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-08-03 18:31:27 +0100

    edit app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/registration",
+    callbackURL: "https://agile-oasis-39774.herokuapp.com/auth/google/registration",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
