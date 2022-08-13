5f9f7176d6974cc59331104f796094d4bf35aea8
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-08-13 02:56:15 +0100

    major correction

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 
@@ -45,26 +45,26 @@ const Registration = new mongoose.model("Registration", registrationSchema);
 
 passport.use(Registration.createStrategy());
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+passport.serializeUser(function(registration, done) {
+  done(null, registration.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
+  Registration.findById(id, function(err, registration) {
+    done(err, registration  );
   });
 });
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://agile-oasis-39774.herokuapp.com/auth/google/registration",
+    callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
-    Registration.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
+    Registration.findOrCreate({ googleId: profile.id }, function (err, registration) {
+      return cb(err, registration);
     });
   }
 ));
@@ -76,7 +76,7 @@ app.get("/", function(req, res){
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
-app.get("/auth/google/registration", 
+app.get("/auth/google/secrets", 
   passport.authenticate("google", { failureRedirect: "/login" }),
   function(req, res) {
     // Successful authentication, redirect home.
@@ -92,34 +92,38 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    }else {
-        res.redirect("/login");
-    }
-});
-
-app.get("/submit", function(req, res){
-  if (req.isAuthenticated()){
-    res.render("submit");
-}else {
-    res.redirect("/login");
-}
-});
-
-app.post("/submit", function(req, res){
-  const submittedSecret = req.body.secret;
-  console.log(req.registration.id);
-
-  Registration.findById(req.registration.id, function(err, foundRegistration){
+  Registration.find({"secret": {$ne: null}}, function(err, foundUsers){
     if (err){
       console.log(err);
     }else{
-      if (foundRegistration){
-        foundRegistration.secret = submittedSecret;
-        foundRegistration.save(function(){
+      if (foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+ 
+app.get("/submit", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res){
+  const submitedSecret = req.body.secret;
+
+ // console.log(req.user.id);
+  Registration.findById(req.user.id, function(err, foundUser){
+    if (err){
+      console.log(err);
+    }else{
+      if (foundUser){
+        foundUser.secret = submitedSecret;
+        foundUser.save(function(){
           res.redirect("/secrets");
-        });
+        })
       }
     }
   });
